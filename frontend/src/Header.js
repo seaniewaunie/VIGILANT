e997c113479620aac0ea0060b79880895a9ea307
seaniewaunie <sean.r.graff@gmail.com>
2018-03-07 18:21:14 -0500

    created skeleton for header in frontend

---
+++ frontend/src/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="w3-container">
+        <ul>
+            <li><img src={logo} className="Header-logo" alt="logo" /></li>
+            <li><a href = "#">VIGILANT</a></li>
+            <li><button>Add Visualization</button></li>
+            <li style={{float:"right"}}><button>Log In</button></li>
+
+            <li style={{float:"right"}}><button>Global Filter</button></li>
+            <li style={{float:"right"}}><button>Restore</button></li>
+            <li style={{float:"right"}}><button>Remove</button></li>
+
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './Header';
 import registerServiceWorker from './registerServiceWorker';
 
+ReactDOM.render(<Header />, document.getElementById('header'));
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
