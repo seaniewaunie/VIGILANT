5f035779facfed99f5b0ddba53545691d8fe9a78
Randolph Tan <rantan1@umbc.edu>
2018-04-19 16:57:17 -0400

    cron job

--- backend/ingester/cronDl.sh
+++ backend/ingester/cronDl.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 
+cd ~/vigilant/backend/ingester
 
-wget --continue --output-document=~/4ih5-d5d5.json 'https://data.baltimorecity.gov/resource/4ih5-d5d5.json?$limit=30000000'
+wget --continue --output-document=4ih5-d5d5.json 'https://data.baltimorecity.gov/resource/4ih5-d5d5.json?$limit=30000000'
 
-python ~/vigilant/backend/ingester/productionIngester.py
+python ingester.py
 
 rm "4ih5-d5d5.json"
