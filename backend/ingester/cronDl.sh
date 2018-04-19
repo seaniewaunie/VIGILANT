1b1d4f52156e7d186ffbd4fd0feec93db60e7d03
Randolph Tan <rantan1@umbc.edu>
2018-04-19 16:42:54 -0400

    fixed for server

--- backend/ingester/cronDl.sh
+++ backend/ingester/cronDl.sh
@@ -4,6 +4,6 @@
 
 wget --continue --output-document=4ih5-d5d5.json 'https://data.baltimorecity.gov/resource/4ih5-d5d5.json?$limit=30000000'
 
-python ~/vigilant/backend/ingester/ingester.py
+python ~/vigilant/backend/ingester/productionIngester.py
 
 rm "4ih5-d5d5.json"
