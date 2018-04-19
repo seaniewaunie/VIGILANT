4036ef0efa7dfcad284aa6d022951fcd86272c8d
Randolph Tan <rantan1@umbc.edu>
2018-04-19 16:25:05 -0400

    entry point for ingester

--- backend/ingester/dl.sh
+++ backend/ingester/dl.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+rm "4ih5-d5d5.json"
 
-wget --continue --timestamping --output-document=4ih5-d5d5.json 'https://data.baltimorecity.gov/resource/4ih5-d5d5.json?$limit=300000'
+wget --continue --output-document=4ih5-d5d5.json 'https://data.baltimorecity.gov/resource/4ih5-d5d5.json?$limit=30000000'
+
+python3 ingester.py
